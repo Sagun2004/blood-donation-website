629b9d7a030365bfa37869dfe17bef4bbd3decdc
sagun rajput <sgnrajput@gmail.com>
2026-06-09 11:26:39 +0530

    Updated server port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const jwt     = require('jsonwebtoken');
 const { stmts } = require('./database');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const JWT_SECRET = 'blood_donation_2026_secret';
 const ADMIN_KEY = 'Admin@2026';
 
